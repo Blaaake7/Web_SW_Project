414ae922bbb5bac9d08255ff3b6e6704e230c4e0
Young JIn <leesu0229@naver.com>
2023-12-22 13:01:17 +0900

    admin 페이지 개발

--- admin/src/pages/Reservation.jsx
+++ admin/src/pages/Reservation.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import styles from './Reservation.module.css'; // import your CSS file
 
 export default function Reservation() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    axios.get('http://43.202.228.228:8080/admin/reservation')
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      })
+  }, []);
+
   return (
-    <div>Reservation</div>
-  )
+    <div className={styles.reservationContainer}>
+      <h2>예약 정보 확인</h2>
+      {data && data.map((each, idx) => (
+        <div key={idx} className={styles.reservationItem}>
+          <div className={styles.indexColumn}>{idx + 1}.</div>
+          <div className={styles.infoColumn}>
+            <div className={styles.customerInfo}>{each.customerName}</div>
+          </div>
+          <div className={styles.phoneNumber}>{each.phoneNumber}</div>
+          <div className={styles.dateColumn}>{each.date}</div>
+          <div className={styles.priceColumn}>{each.totalPrice}원</div>
+        </div>
+      ))}
+    </div>
+  );
 }
--- admin/src/pages/Service/Service.jsx
+++ admin/src/pages/Service/Service.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Service.module.css'
 import { useEffect } from 'react'
 import axios from 'axios'
@@ -9,29 +9,71 @@ export default function Service() {
   
     const navigate = useNavigate();
 
-
+    const [data, setData] = useState([]);
   useEffect(() => {
     axios.get('http://43.202.228.228:8080/service')
     .then((res) => {
-        console.log(res.data)
+      console.log(res.data);
+      setData(res.data);
     })
     .catch((err) => {
-        console.log(err);
+      console.log(err);
+  })
+  }, []);
+  
+  const handleDelete = (id) => {
+    axios.post('http://43.202.228.228:8080/admin/deleteService', {
+      serviceId: id
     })
-  }, [])
+    .then((res) => {
+      axios.get('http://43.202.228.228:8080/service')
+      .then((res) => {
+        alert('삭제하였습니다')
+        const newData = [...res.data]
+        setData(newData);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    })
+  }
 
     return (
     <div>
-        <div className={styles.header}>
-			<h2> 서비스 관리 </h2>
+      <div className={styles.header} style={{display: "flex"}}>
+        <div>
+			    <h2> 서비스 관리 </h2>
+          <div>
+			    	<button className={styles.button}
+			    			onClick={() => navigate("/service/create")}
+			    	>
+			    		생성하기
+			    	</button>
+			    </div>
+        </div>
+		  </div>
+      {data && data.map((each, idx) => (
+        <div key={idx} style={{display: "flex", margin: "24px 250px", border: "1px solid gray", padding: "20px"}}>
+          <img src={each.picture} alt='서비스 이미지' style={{marginLeft: "48px"}}/> 
+          <div style={{marginLeft: "48px"}}>
             <div>
-				<button className={styles.button}
-						onClick={() => navigate("/service/create")}
-				>
-					생성하기
-				</button>
-			</div>
-		</div>
+              {each.serviceName}
+            </div>
+            <div>
+              {each.price}원
+            </div>
+            <div>
+              {each.timeTaken}분
+            </div>
+            <div>
+              {each.info}
+            </div>
+          </div>
+          <button onClick={() => {handleDelete(each.serviceId)}} className={styles.deleteButton}>
+            삭제
+          </button>
+        </div>
+        ))}
     </div>
   )
 }
